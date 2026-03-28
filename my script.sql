eecc6a8bdc54d198e9408aaf0437f79a53f743f0
Bright Okotie <okobright2015@gmail.com>
2026-03-28 23:46:39 +0000

    Add files via upload

--- my script.sql
+++ my script.sql
@@ -247,4 +247,222 @@ LEFT JOIN papers ON papers.student_id = students.id
 GROUP BY first_name
 ORDER BY Average DESC;
 
-;;;;;;;
+CREATE TABLE reviewers 
+	(id INT PRIMARY KEY AUTO_INCREMENT, 
+	 first_name VARCHAR(50) NOT NULL,
+     last_name VARCHAR(50) NOT NULL
+     );
+     
+CREATE TABLE series
+	(id INT PRIMARY KEY AUTO_INCREMENT,
+     title VARCHAR(100),
+     released_year YEAR,
+     genre VARCHAR(100)
+     );
+     
+CREATE TABLE reviews
+	(id INT PRIMARY KEY AUTO_INCREMENT,
+     rating DECIMAL(2,1),
+     series_id INT,
+     reviewer_id INT,
+     FOREIGN KEY (series_id) REFERENCES series(id),
+     FOREIGN KEY (reviewer_id) REFERENCES reviewers(id)
+     );
+     
+INSERT INTO series (title, released_year, genre)
+VALUES ('Archer', 2009, 'Animation'),
+	   ('Arrested Development', 2003, 'Comedy'),
+       ('Bob\'s Burgers', 2011, 'Animation'),
+       ('Bojack Horseman', 2014, 'Animation'),
+       ('Breaking Bad', 2008, 'Drama'),
+       ('Curb Your Enthusiasm', 2000, 'Comedy'),
+       ('Fargo', 2014, 'Drama'),
+       ('Freaks and Geeks', 1999, 'Comedy'),
+       ('General Hospital', 1963, 'Drama'),
+       ('Halt and Catch Fire', 2014, 'Drama'),
+       ('Malcolm In The Middle', 2000, 'Comedy'),
+       ('Pushing Daisies', 2007, 'Comedy'),
+       ('Seinfeld', 1989, 'Comedy'),
+       ('Stranger Things', 2016, 'Drama');
+       
+USE tv_db;
+SHOW TABLES;
+
+INSERT INTO reviewers (first_name, last_name)
+VALUES ('Thomas', 'Stoneman'),
+	   ('Wyatt', 'Skaggs'),
+       ('Kimbra', 'Masters'),
+       ('Domingo', 'Cortes'),
+       ('Colt', 'Steele'),
+       ('Pinkie', 'Petit'),
+       ('Marlon', 'Crafford');
+       
+INSERT INTO reviews (series_id, reviewer_id, rating)
+VALUES (1, 1, 8.0), (1, 2, 7.5), (1, 3, 8.5), (1, 4, 7.7), (1, 5, 8.9),
+	   (2, 1, 8.1), (2, 4, 6.0), (2, 3, 8.0), (2, 6, 8.4), (2, 5, 9.9),
+       (3, 1, 7.0), (3, 6, 7.5), (3, 4, 8.0), (3, 3, 7.1), (3, 5, 8.0),
+       (4, 1, 7.5), (4, 3, 7.8), (4, 4, 8.3), (4, 2, 7.6), (4, 5, 8.5),
+       (5, 1, 9.5), (5, 3, 9.0), (5, 4, 9.1), (5, 2, 9.3), (5, 5, 9.9),
+       (6, 2, 6.5), (6, 3, 7.8), (6, 4, 8.8), (6, 2, 8.4), (6, 5, 9.1),
+       (7, 2, 9.1), (7, 5, 9.7), (8, 4, 8.5), (8, 2, 7.8), (8, 6, 8.8),
+       (8, 5, 9.3), (9, 2, 5.5), (9, 3, 6.8), (9, 4, 5.8), (9, 6, 4.3),
+       (9, 5, 4.5), (10, 5, 9.9), (13, 3, 8.0), (13, 4, 7.2), (14, 2, 8.5),
+       (14, 3, 8.9), (14, 4, 8.9);
+       
+SELECT * FROM series;
+SELECT * FROM reviewers;
+SELECT * FROM reviews;
+
+SELECT title, rating FROM series
+JOIN reviews ON reviews.series_id = series.id
+LIMIT 15;
+
+SELECT title, ROUND(AVG(rating), 2) AS Avg_rating FROM series
+JOIN reviews ON reviews.series_id = series.id
+GROUP BY title
+ORDER BY Avg_rating ASC;
+
+SHOW DATABASES;USE tv_db;
+SELECT first_name, last_name, rating FROM reviewers
+JOIN reviews ON reviews.reviewer_id = reviewers.id;
+
+SELECT * FROM reviews, series;
+
+SELECT * FROM series
+JOIN reviews ON reviews.series_id = series.id;
+
+SELECT * FROM series
+LEFT JOIN reviews ON reviews.series_id = series.id;
+
+SELECT * FROM series
+LEFT JOIN reviews ON reviews.series_id = series.id
+WHERE rating IS NULL;
+
+SELECT title AS unreviewed_series FROM series
+LEFT JOIN reviews ON reviews.series_id = series.id
+WHERE rating IS NULL;
+
+SELECT title AS unreviewed_series FROM reviews
+RIGHT JOIN series ON reviews.series_id = series.id
+WHERE rating IS NULL;
+
+SELECT genre, ROUND(AVG(rating), 2) AS avg_rating FROM series
+JOIN reviews ON reviews.series_id = series.id
+GROUP BY genre;
+
+SELECT first_name, last_name, COUNT(rating) AS COUNT, IFNULL(MIN(rating), 0) AS MIN, IFNULL(MAX(rating), 0) AS MAX, IFNULL(AVG(rating), 0) AS AVG,
+	CASE
+		 WHEN COUNT(rating) >= 10 THEN 'POWERUSER'
+		 WHEN COUNT(rating) > 0 THEN 'ACTIVE'
+		 ELSE 'INACTIVE'
+	END AS status
+FROM reviewS
+RIGHT JOIN reviewers ON reviews.reviewer_id = reviewers.id
+GROUP BY first_name, last_name;
+
+SELECT title, rating, CONCAT(first_name, ' ', last_name) AS reviewer FROM reviews
+JOIN series ON reviews.series_id = series.id
+JOIN reviewers ON reviews.reviewer_id = reviewers.id
+ORDER BY title;
+
+
+SELECT title, released_year, rating, genre, first_name, last_name FROM reviews
+JOIN series ON series.id = reviews.series_id
+JOIN reviewers ON reviewers.id = reviews.reviewer_id;
+
+CREATE VIEW full_reviews AS
+SELECT title, released_year, rating, genre, first_name, last_name FROM reviews
+JOIN series ON series.id = reviews.series_id
+JOIN reviewers ON reviewers.id = reviews.reviewer_id;
+
+DROP VIEW full_reviews;
+
+SHOW TABLES;
+
+SELECT * FROM full_reviews
+WHERE genre = 'comedy';
+
+SELECT * FROM full_reviews;
+
+SELECT title, AVG(rating) FROM full_reviews
+GROUP BY title HAVING count(rating) >1;
+
+SELECT title, AVG(rating) FROM full_reviews
+GROUP BY title WITH ROLLUP;
+
+SELECT title, COUNT(rating) FROM full_reviews
+GROUP BY title WITH ROLLUP;
+
+SELECT title, genre, AVG(rating) FROM full_reviews
+GROUP BY title, genre WITH ROLLUP;
+
+SELECT first_name, released_year, genre, AVG(rating) FROM full_reviews
+GROUP BY released_year, genre, first_name WITH ROLLUP;
+
+SELECT @@GLOBAL.sql_mode;
+SELECT @@SESSION.sql_mode;
+
+SELECT * FROM full_reviews;
+
+SELECT title, genre, AVG(rating) OVER(PARTITION BY genre) AS HA FROM full_reviews;
+
+SELECT title, genre, AVG(rating) AS HA FROM full_reviews
+GROUP BY title, genre;
+
+CREATE DATABASE window_funcs;
+
+USE window_funcs;
+
+CREATE TABLE employees (emp_no INT PRIMARY KEY AUTO_INCREMENT, department VARCHAR(20), salary INT);
+
+INSERT INTO employees (department, salary)
+VALUES ('engineering', 80000),
+	   ('engineering', 69000),
+       ('engineering', 70000),
+       ('engineering', 103000),
+       ('engineering', 67000),
+       ('engineering', 89000),
+       ('engineering', 91000),
+       ('sales', 59000),
+       ('sales', 70000),
+       ('sales', 159000),
+       ('sales', 72000),
+       ('sales', 60000),
+       ('sales', 61000),
+       ('sales', 61000),
+       ('customer service', 38000),
+       ('customer service', 45000),
+       ('customer service', 61000),
+       ('customer service', 40000),
+       ('customer service', 31000),
+       ('customer service', 56000),
+	   ('customer service', 55000);
+       SHOW DATABASES;
+	SELECT DATABASE();
+SHOW TABLES;
+SELECT * FROM employees;
+
+SELECT department, salary, AVG(salary) FROM employees
+GROUP BY department, salary WITH ROLLUP;
+
+SELECT emp_no, department, salary, AVG(salary) OVER(PARTITION BY department) AS rate FROM employees;
+
+SELECT emp_no, department, salary, AVG(salary) OVER(PARTITION BY department) AS dept_avg, 
+AVG(salary) OVER() AS comp_avg
+FROM employees;
+SELECT emp_no, department, salary, SUM(salary) OVER(PARTITION BY department) AS dept_payroll, SUM(salary) OVER() AS Pay_roll FROM employees;  
+
+SELECT emp_no, department, salary, SUM(salary) OVER(PARTITION BY department ORDER BY salary) AS rolling_payroll, SUM(salary) OVER() AS total_payroll FROM employees;     
+DESC employees;
+
+SELECT emp_no, department, salary, SUM(salary) OVER(PARTITION BY department ORDER BY salary) AS rolling_payroll, SUM(salary) OVER(PARTITION BY department) AS total_payroll FROM employees;
+
+SELECT emp_no, department, salary, RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS rolling_payroll, RANK() OVER(ORDER BY salary DESC) AS total_payroll FROM employees ORDER BY department;
+
+SELECT emp_no, department, salary, FIRST_VALUE(emp_no) OVER(PARTITION BY department ORDER BY salary DESC) AS highestpaid_dept, FIRST_VALUE(emp_no) OVER(ORDER BY salary DESC) AS highestpay_overall FROM employees;
+
+SELECT emp_no, department, salary, salary - LAG(salary) OVER(ORDER BY salary DESC) AS salary_diff FROM employees;
+
+SELECT emp_no, department, salary, salary - LEAD(salary) OVER(ORDER BY salary DESC) AS salary_diff FROM employees;
+
+SELECT emp_no, department, salary, salary - LAG(salary) OVER(PARTITION BY department ORDER BY salary DESC) AS deptsalary_diff FROM employees;
